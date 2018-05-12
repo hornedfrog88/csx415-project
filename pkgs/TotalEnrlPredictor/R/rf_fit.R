810062cff76963849745acc372fbc81bc44e5d26
Rich McGowan <rmcgowan@berkeley.edu>
2018-05-12 12:15:43 -0700

    Update-20180512

---
+++ pkgs/TotalEnrlPredictor/R/rf_fit.R
@@ -0,0 +1,14 @@
+#' This function takes a data frame of summary course enrollments
+#' and returns a Random Forest Model object for the purpose of
+#' predicting total Course Enrollments for a given Subject Area
+#' (e.g. Graduate Data Science courses)
+#'
+#' @param course_set A data frame containing the required 7 columns
+#' @return random forest model object
+#' @export
+#' @examples
+#' rf_fit(course_set)
+rf_fit <- function(course_set) {
+rf_fit <- randomForest::randomForest(EnrlTotal~.,data= ourse_set,mtry=3,ntree=10)
+  return(rf_fit)
+}
---
+++ pkgs/TotalEnrlPredictor/R/samplecoursedata.R
@@ -0,0 +1,16 @@
+#' Sample Course Data
+#'
+#' Combined Course Enrollment Statistics from Fall 2016 thru Summer 2018
+#'
+#'
+#' @format A data frame with 24390 rows and 7 variables:
+#' \describe{
+#'   \item{Term}{Course Schedule/Enrollment Term}
+#'   \item{CourseID}{Unique Course Identifier}
+#'   \item{Subject}{Subject Area of the Course}
+#'   \item{MaxUnits}{Maximum Unit Value for the Course}
+#'   \item{TotalCapacity}{Total Combined Class Section Capacity}
+#'   \item{EnrlTotal}{Total Combined Class Section Enrollment}
+#'   \item{TotalSections}{Total Scheduled Sections for the Course}
+#'   }
+"samplecoursedata"
