bfd3bd1a8d469636f7c49180263c24ac0c1979fe
Rich McGowan <rmcgowan@berkeley.edu>
2018-05-27 17:50:09 -0700

    Update-20180527

--- src/rf_fit.R
+++ src/rf_fit.R
@@ -10,9 +10,9 @@ setwd("C:/Users/Richard/Desktop/csx415-project")
 #Read in the entire data set
 sample_course_data <- read_csv("./data/SampleCourseData.csv")
 
-#Filter Out High and Low Enrolled Courses
+#Filter Out Under and Over Enrolled Courses
 course_set <- sample_course_data %>% filter((EnrlTotal) > 1,(TotalCapacity > 0))
-course_set <- course_set %>% filter((EnrlTotal/TotalCapacity) >.75)
+course_set <- course_set %>% filter((EnrlTotal/TotalCapacity) >.75,(EnrlTotal/TotalCapacity)<1.10)
 setDT(course_set)
 
 #Save the model formula to a variable called form
